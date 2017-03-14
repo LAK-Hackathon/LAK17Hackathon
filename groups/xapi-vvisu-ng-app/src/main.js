e23fe3d5de443218824543f1851a1f9b3db788b6
Mohamed Ez zaouia <ezzaouia.mohamed@gmail.com>
2017-03-14 17:12:29 +0100

    adding the gn sample web app into group3

--- groups/xapi-vvisu-ng-app/src/main.js
+++
@@ -1,6 +0,0 @@
-import { bootstrap } from 'angular';
-import { element } from 'angular';
-import app from  './app'
-import './styles.scss';
-
-element(document).ready(() => bootstrap(document, [app.name]));
